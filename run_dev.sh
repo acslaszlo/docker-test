cf8639936bbb2c29cee2134dc8859c9000bde1b0
Laszlo Acs <acs.laci@gmail.com>
2018-11-26 04:12:58 -0800

    Closes #2: Added a dev service (which reloads on local code change).

---
+++ run_dev.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+COMPOSE_BASE_FILE='docker-compose-base.yml'
+COMPOSE_SERVICE_FILE='docker-compose-dev.yml'
+
+function msg() {
+    echo
+    echo "======================================="
+    echo "  $1"
+    echo "======================================="
+    echo
+}
+
+function finish() {
+    msg "Stopping docker compose"
+    docker-compose -f "${COMPOSE_BASE_FILE}" -f "${COMPOSE_SERVICE_FILE}" down
+}
+
+trap finish EXIT
+
+. test-venv/bin/activate
+pip install -r tests/requirements.txt
+
+msg "Starting local db"
+docker-compose -f "${COMPOSE_BASE_FILE}" up --build -d
+
+msg "Initializing the db"
+PYTHONPATH=. python tests/init_db.py
+
+msg "Staring docker compose"
+docker-compose -f "${COMPOSE_BASE_FILE}" -f "${COMPOSE_SERVICE_FILE}" up --build
